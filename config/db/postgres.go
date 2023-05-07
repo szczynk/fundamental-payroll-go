74ed0e1bea8aa0f353460b4f16b0f60c1534d072
szczynk <34181070+szczynk@users.noreply.github.com>
2023-05-07 11:19:00 +0700

    Update postgres.go

--- config/db/postgres.go
+++ config/db/postgres.go
@@ -2,19 +2,18 @@ package db
 
 import (
 	"database/sql"
-	"fundamental-payroll-go/config"
 	"fundamental-payroll-go/helper/apperrors"
 	"time"
 
 	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
-func NewPgxDatabase(cfg *config.Config) (*sql.DB, error) {
-	if cfg.Database.URL == "" {
+func NewPgxDatabase(driver, url string) (*sql.DB, error) {
+	if url == "" {
 		return nil, apperrors.New(apperrors.ErrDbUrlNotFound)
 	}
 
-	db, err := sql.Open(cfg.Database.Driver, cfg.Database.URL)
+	db, err := sql.Open(driver, url)
 	if err != nil {
 		return nil, err
 	}
