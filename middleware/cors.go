9281ad700b097c4b2f9bcbe98d050548a75a3def
szczynk <34181070+szczynk@users.noreply.github.com>
2023-05-07 13:49:13 +0700

    Update cors.go

--- middleware/cors.go
+++ middleware/cors.go
@@ -10,7 +10,11 @@ func Cors(w http.ResponseWriter, r *http.Request, next http.Handler) http.Handle
 		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PUT, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-CSRF-Token")
 		if r.Method == "OPTIONS" {
-			w.Write([]byte("allowed"))
+			_, err := w.Write([]byte("allowed"))
+			if err != nil {
+				http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
 			return
 		}
 		next.ServeHTTP(w, r)
